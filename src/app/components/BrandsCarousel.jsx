4d1f44eb2dd5ed9bd546423230996dace96b78a0
Santiago PEREZ <137639404+santiperez98@users.noreply.github.com>
2024-11-28 19:34:55 -0300

    nav act y services

--- src/app/components/BrandsCarousel.jsx
+++ src/app/components/BrandsCarousel.jsx
@@ -1,20 +1,27 @@
 import { motion } from 'framer-motion';
 import Image from 'next/image';
+import image1 from '../../../public/rio.png';
+import image2 from '../../../public/nyg.png';
+import image3 from '../../../public/reba.png';
+import image4 from '../../../public/cera.png';
+import image5 from '../../../public/ladia.png';
 
 const BrandsCarousel = () => {
-  const brands = ['/images/marca1.png', '/images/marca2.png', '/images/marca3.png'];
+  const brands = [image1, image2, image3, image4, image5];
 
   return (
     <div className="overflow-hidden">
       <motion.div className="flex space-x-8 animate-marquee">
         {brands.map((src, index) => (
-          <Image 
-            src={src} 
-            key={index} 
-            alt={`Marca ${index + 1}`} 
-            width={128} // Cambia estos valores según las dimensiones de tus imágenes
-            height={64} 
-          />
+          <div key={index} className="flex-shrink-0">
+            <Image 
+              src={src} 
+              alt={`Marca ${index + 1}`} 
+              width={128} // Establece un tamaño base para las imágenes
+              height={64} // Establece un tamaño base para las imágenes
+              className="object-contain" // Hace que las imágenes se ajusten bien dentro de su contenedor
+            />
+          </div>
         ))}
       </motion.div>
     </div>
